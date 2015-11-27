3e99d6c3a9faed4f0ae48e3ccb7cd43e6e1bab25
Alex Manelis <amanelis@gmail.com>
2015-11-27 13:18:20 -0800

    Few basic specs for time being

--- spec/proc_index_spec.rb
+++ spec/proc_index_spec.rb
@@ -3,8 +3,18 @@ require 'proc_index'
 
 describe ProcIndex do
   describe '.ps' do
-    context 'returns true' do
-      it { expect(ProcIndex.ps).to_not be_nil }
+    context 'returns an instance of ProcIndex::Container' do
+      subject { ProcIndex.ps }
+      it { expect(subject).to_not be_nil }
+      it { expect(subject.body).to_not be_nil }
+    end
+  end
+
+  describe '.search' do
+    context 'returns a collection of Hashie::Mash objects' do
+      subject { ProcIndex.search('ruby') }
+      it { expect(subject).to_not be_nil }
+      it { expect(subject.count).to be > 1}
     end
   end
 end
